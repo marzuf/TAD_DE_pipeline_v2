f02bcea6228c94b3368592d5085688da127af5b7
mzuer <z_marie_7@hotmail.com>
2020-08-22 22:26:31 +0200

    22.8.20_1

---
+++ 10sameNbrTopDom_runEmpPvalMeanTADCorr.R
@@ -0,0 +1,147 @@
+#!/usr/bin/Rscript
+
+options(scipen=100)
+
+startTime <- Sys.time()
+
+#sameNbrTopDom -> take correlation values for random model from v2_Yuanlong_Cancer_TAD_DA folder
+# (otherwise not enough values for the moment)
+
+################  USE THE FOLLOWING FILES FROM PREVIOUS STEPS
+# - script4: all_meanCorr_TAD.Rdata
+# - script7sameNbr: meanCorr_sample_around_TADs_sameNbr.Rdata
+################################################################################
+
+################  OUTPUT
+# - emp_pval_meanCorr.Rdata
+################################################################################
+
+SSHFS <- F
+setDir <- ifelse(SSHFS, "/media/electron", "")
+
+args <- commandArgs(trailingOnly = TRUE)
+stopifnot(length(args) == 1)
+settingF <- args[1]
+stopifnot(file.exists(settingF))
+
+pipScriptDir <- paste0(setDir, "/mnt/ed4/marie/scripts/TAD_DE_pipeline_v2")
+
+script0_name <- "0_prepGeneData"
+script1_name <- "1_runGeneDE"
+script4_name <- "4_runMeanTADCorr"
+script7sameNbr_name <- "7sameNbr_runPermutationsMeanTADCorr"
+script_name <- "10sameNbr_runEmpPvalMeanTADCorr"
+stopifnot(file.exists(paste0(pipScriptDir, "/", script_name, ".R")))
+cat(paste0("> START ", script_name,  "\n"))
+
+source("main_settings.R")
+#source("run_settings.R")
+source(settingF)
+source(paste0(pipScriptDir, "/", "TAD_DE_utils.R"))
+suppressPackageStartupMessages(library(foreach, warn.conflicts = FALSE, quietly = TRUE, verbose = FALSE))
+suppressPackageStartupMessages(library(doMC, warn.conflicts = FALSE, quietly = TRUE, verbose = FALSE))
+
+registerDoMC(ifelse(SSHFS,2, nCpu)) # loaded from main_settings.R
+
+# if microarray was not set in the settings file -> by default set to  FALSE
+if(!exists("microarray")) microarray <- FALSE
+
+
+# create the directories
+curr_outFold <- paste0(pipOutFold, "/", script_name)
+system(paste0("mkdir -p ", curr_outFold))
+
+pipLogFile <- paste0(pipOutFold, "/", format(Sys.time(), "%Y%d%m%H%M%S"),"_", script_name, "_logFile.txt")
+system(paste0("rm -f ", pipLogFile))
+
+# ADDED 16.11.2018 to check using other files
+txt <- paste0("inputDataType\t=\t", inputDataType, "\n")
+printAndLog(txt, pipLogFile)
+txt <- paste0("gene2tadDT_file\t=\t", gene2tadDT_file, "\n")
+printAndLog(txt, pipLogFile)
+txt <- paste0("TADpos_file\t=\t", TADpos_file, "\n")
+printAndLog(txt, pipLogFile)
+txt <- paste0("settingF\t=\t", settingF, "\n")
+printAndLog(txt, pipLogFile)
+
+
+corr_type <- "meanCorr"
+txt <- paste0("taking sample correlation for corr_type\t=\t", settingF, "\n")
+printAndLog(txt, pipLogFile)
+
+### RETRIEVE ALL THE FILES IN THE FOLDER !!!
+mainPipFold <- dirname(dirname(pipOutFold))
+## HACK HERE 22.08.2020 -> To have corr values for all datasets -> take the same as YL data
+mainPipFold <- "/mnt/etemp/marie/v2_Yuanlong_Cancer_HiC_data_TAD_DA/PIPELINE/OUTPUT_FOLDER"
+
+txt <- paste0("!!! take all the files matching \"meanCorr_sample_around_TADs_sameNbr.Rdata\" in ", mainPipFold, "\n")
+printAndLog(txt, pipLogFile)
+
+all_sampleCorr_files <- list.files(mainPipFold, pattern="meanCorr_sample_around_TADs_sameNbr.Rdata", full.names = TRUE, recursive = TRUE)
+
+all_sampleCorr_files <- all_sampleCorr_files[grepl(script7sameNbr_name, all_sampleCorr_files)]  ### added 26.11.2019 otherwise match also the files for partial corr.
+all_sampleCorr_files <- all_sampleCorr_files[!grepl("RANDOM", all_sampleCorr_files) & !grepl("PERMUT", all_sampleCorr_files)]
+
+all_hicds <- list.files(mainPipFold)
+all_hicds <- all_hicds[!grepl("RANDOM", all_hicds) & !grepl("PERMUT", all_hicds)]
+
+all_exprds <- sapply(all_hicds, function(x) list.files(file.path(mainPipFold, x)))
+
+
+txt <- paste0("sampleCorr_files used (n=", length(all_sampleCorr_files), "):\n")
+printAndLog(txt, pipLogFile)
+txt <- paste0(all_sampleCorr_files, collapse="\n")
+printAndLog(txt, pipLogFile)
+
+
+
+stopifnot(length(all_sampleCorr_files) == length(unlist(all_exprds)))
+
+
+### PREPARE THE SAMPLE CORR VALUES FROM ALL DATASETS
+corr_file="/media/electron/mnt/etemp/marie/Yuanlong_Cancer_HiC_data_TAD_DA/PIPELINE/OUTPUT_FOLDER/GSE105381_HepG2_40kb/TCGAlihc_norm_lihc/7sameNbr_runPermutationsMeanTADCorr/meanCorr_sample_around_TADs_sameNbr.Rdata"
+all_sample_corrValues <- foreach(corr_file = all_sampleCorr_files, .combine='c') %dopar% {
+  stopifnot(file.exists(corr_file))
+  corr_data <- eval(parse(text = load(corr_file)))
+  all_samp_corrs <- as.numeric(sapply(corr_data, function(x) x[[paste0(corr_type)]]))
+  stopifnot(!is.null(all_samp_corrs))
+  all_samp_corrs <- na.omit(all_samp_corrs)  
+  all_samp_corrs
+}
+
+
+# RETRIEVE THE OBSERVED CORR DATA
+obs_corr_file <- file.path(pipOutFold, script4_name, "all_meanCorr_TAD.Rdata")
+stopifnot(file.exists(obs_corr_file))
+all_obs_corr <- eval(parse(text = load(obs_corr_file)))
+ 
+emp_pval_meanCorr <- sapply(all_obs_corr, function(x) {
+  (sum(all_sample_corrValues >= x) + 1)/(length(all_sample_corrValues) + 1)
+})
+names(emp_pval_meanCorr) <- names(all_obs_corr)
+stopifnot(all(emp_pval_meanCorr > 0 & emp_pval_meanCorr <= 1 ))
+
+
+### CHECK RETRIEVE THE RIGHT VALUES
+tadListFile <- file.path(pipOutFold, script0_name, "pipeline_regionList.Rdata")
+stopifnot(file.exists(tadListFile))
+pipeline_tadList <- eval(parse(text = load(tadListFile))) # not adjusted
+stopifnot(setequal(names(emp_pval_meanCorr), pipeline_tadList))
+
+
+outFile <- file.path(curr_outFold, "emp_pval_meanCorr.Rdata")
+save(emp_pval_meanCorr, file= outFile)
+cat(paste0("... written: ", outFile, "\n"))
+
+#############################################################################################################################
+#############################################################################################################################
+#############################################################################################################################
+
+
+txt <- paste0(startTime, "\n", Sys.time(), "\n")
+printAndLog(txt, pipLogFile)
+cat(paste0("*** DONE: ", script_name, "\n"))
+       
+          
+        
+        
---
+++ 9pF_runEmpPvalMeanTADLogFC.R
@@ -0,0 +1,210 @@
+#!/usr/bin/Rscript
+
+options(scipen=100)
+
+startTime <- Sys.time()
+
+################  USE THE FOLLOWING FILES FROM PREVIOUS STEPS
+# - script3: all_meanLogFC_TAD.Rdata
+# - script6: meanLogFC_permDT.Rdata
+################################################################################
+
+################  OUTPUT
+# - emp_pval_meanLogFC.Rdata + plots
+################################################################################
+
+SSHFS <- F
+setDir <- ifelse(SSHFS, "/media/electron", "")
+
+args <- commandArgs(trailingOnly = TRUE)
+stopifnot(length(args) == 1)
+settingF <- args[1]
+stopifnot(file.exists(settingF))
+
+pipScriptDir <- paste0(setDir, "/mnt/ed4/marie/scripts/TAD_DE_pipeline_v2")
+
+script0_name <- "0_prepGeneData"
+script1_name <- "1_runGeneDE"
+script3_name <- "3_runMeanTADLogFC"
+script6_name <- "6pF_runPermutationsMeanLogFC" # <<< 19.08.20: add purity filter !!!
+script_name <- "9pF_runEmpPvalMeanTADLogFC" # <<< 19.08.20: add purity filter !!!
+stopifnot(file.exists(paste0(pipScriptDir, "/", script_name, ".R")))
+cat(paste0("> START ", script_name,  "\n"))
+
+source("main_settings.R")
+source(settingF)
+source(paste0(pipScriptDir, "/", "TAD_DE_utils.R"))
+suppressPackageStartupMessages(library(foreach, warn.conflicts = FALSE, quietly = TRUE, verbose = FALSE))
+suppressPackageStartupMessages(library(doMC, warn.conflicts = FALSE, quietly = TRUE, verbose = FALSE))
+registerDoMC(ifelse(SSHFS, 2, nCpu)) # from main_settings.R
+
+
+# create the directories
+curr_outFold <- paste0(pipOutFold, "/", script_name)
+system(paste0("mkdir -p ", curr_outFold))
+
+pipLogFile <- paste0(pipOutFold, "/", format(Sys.time(), "%Y%d%m%H%M%S"),"_", script_name, "_logFile.txt")
+system(paste0("rm -f ", pipLogFile))
+
+# ADDED 27.11.2018 to check using other files
+txt <- paste0("gene2tadDT_file\t=\t", gene2tadDT_file, "\n")
+printAndLog(txt, pipLogFile)
+txt <- paste0("TADpos_file\t=\t", TADpos_file, "\n")
+printAndLog(txt, pipLogFile)
+txt <- paste0("settingF\t=\t", settingF, "\n")
+printAndLog(txt, pipLogFile)
+
+
+# ADDED 16.11.2018 to check using other files
+txt <- paste0("inputDataType\t=\t", inputDataType, "\n")
+printAndLog(txt, pipLogFile)
+txt <- paste0("gene2tadDT_file\t=\t", gene2tadDT_file, "\n")
+printAndLog(txt, pipLogFile)
+txt <- paste0("TADpos_file\t=\t", TADpos_file, "\n")
+printAndLog(txt, pipLogFile)
+txt <- paste0("settingF\t=\t", settingF, "\n")
+printAndLog(txt, pipLogFile)
+
+#****************************************************************************************************************************
+plotType  <- "svg"
+# annotate the plot for the nTop top ranking regions
+nTop <- 10
+#****************************************************************************************************************************
+
+################################****************************************************************************************
+####################################################### PREPARE INPUT
+################################****************************************************************************************
+
+# LOAD DATA AND DISCARD THE REGIONS WITH NA 
+obs_TADLogFC <- eval(parse(text = load(paste0(pipOutFold, "/", script3_name, "/all_meanLogFC_TAD.Rdata"))))
+initLen <- length(obs_TADLogFC)
+obs_TADLogFC <- na.omit(obs_TADLogFC)
+txt <- paste0(toupper(script_name), "> Discard rows with NA in observed logFC. Retain: ", length(obs_TADLogFC), "/", initLen, "\n")
+printAndLog(txt, pipLogFile)
+
+#####>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<>>>>>>>><<< 19.08.20: add purity filter !!!
+curr_exprds <- basename(pipOutFold)
+curr_hicds <- basename(dirname(pipOutFold))
+
+purityFilterFile <- file.path("//mnt/etemp/marie/v2_Yuanlong_Cancer_HiC_data_TAD_DA_PURITYFILTER/PREP_DATA_PURITYFILTER/aran/CPE/log10/TADs_to_keep.Rdata")
+stopifnot(file.exists(purityFilterFile))
+all_TADs_to_keep <- get(load(purityFilterFile))
+
+cat(head(dirname(all_TADs_to_keep)), "\n")
+
+cat(file.path(curr_hicds, curr_exprds), "\n")
+
+ds_TADs_to_keep <- all_TADs_to_keep[dirname(all_TADs_to_keep) == file.path(curr_hicds, curr_exprds)]
+if(length(ds_TADs_to_keep) == 0) stop("--- found no TADs to keep after purity filter ! \n")
+
+obs_TADLogFC <- obs_TADLogFC[names(obs_TADLogFC) %in% basename(ds_TADs_to_keep)]
+stopifnot(length(obs_TADLogFC) > 0)
+
+#####>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<>>>>>>>><<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+
+permut_TADLogFC_DT <- eval(parse(text = load(paste0(pipOutFold, "/", script6_name, "/meanLogFC_permDT.Rdata"))))
+initNrow <- nrow(permut_TADLogFC_DT)
+permut_TADLogFC_DT <- na.omit(permut_TADLogFC_DT)
+txt <- paste0(toupper(script_name), "> Discard rows with NA in permutation logFC. Retain: ", nrow(permut_TADLogFC_DT), "/", initNrow, "\n")
+printAndLog(txt, pipLogFile)
+
+intersectRegions <- sort(intersect(names(obs_TADLogFC), rownames(permut_TADLogFC_DT)))
+txt <- paste0(toupper(script_name), "> Take regions in common between permutations and observed data \n")
+printAndLog(txt, pipLogFile)
+
+initLen <- length(obs_TADLogFC)
+obs_TADLogFC <- obs_TADLogFC[intersectRegions]
+txt <- paste0(toupper(script_name), "> ... -> observed: ", length(obs_TADLogFC), "/", initLen, "\n")
+printAndLog(txt, pipLogFile)
+
+initNrow <- nrow(permut_TADLogFC_DT)
+permut_TADLogFC_DT <- permut_TADLogFC_DT[intersectRegions,]
+txt <- paste0(toupper(script_name), "> ... -> permutations: ", nrow(permut_TADLogFC_DT), "/", initNrow, "\n")
+printAndLog(txt, pipLogFile)
+
+stopifnot(is.numeric(permut_TADLogFC_DT[1,1]))
+
+#>><<< 19.08.20: add purity filter !!! for check pf
+stopifnot(length(obs_TADLogFC) == nrow(permut_TADLogFC_DT))
+
+################################****************************************************************************************
+####################################################### CALCULATE EMP. PVAL LOGFC
+################################****************************************************************************************
+
+### filter TAD only regions
+if(useTADonly) {
+    if( length(grep("_TAD", rownames(permut_TADLogFC_DT))) > 0 ) {
+        txt <- paste0(toupper(script_name), "> !!! WARNING: logFC permutations data generated using non-TAD regions as well !!!\n")
+        printAndLog(txt, pipLogFile)    
+    }
+    initLen <- length(intersectRegions)
+    intersectRegions <- intersectRegions[grep("_TAD", intersectRegions)]
+    txt <- paste0(toupper(script_name), "> Take only TAD regions: ", length(intersectRegions), "/", initLen, "\n")
+    printAndLog(txt, pipLogFile)    
+}
+
+
+emp_pval_meanLogFC <- foreach(reg = intersectRegions, .combine='c') %dopar% {
+  # select the random values for this region
+  shuff_logFC_TAD_region <- permut_TADLogFC_DT[reg,]
+  stopifnot(length(shuff_logFC_TAD_region) == ncol(permut_TADLogFC_DT))
+  # select the observed value for this region
+  obs_meanLogFC <- obs_TADLogFC[reg]
+  stopifnot(length(obs_meanLogFC) == 1 )
+  if(obs_meanLogFC > 0){
+    emp_pval <- sum(shuff_logFC_TAD_region >= obs_meanLogFC)   
+  } else if (obs_meanLogFC < 0) {
+    emp_pval <- sum(shuff_logFC_TAD_region <= obs_meanLogFC)   
+  } else {
+    emp_pval <- mean(c(sum(shuff_logFC_TAD_region <= obs_meanLogFC), sum(shuff_logFC_TAD_region >= obs_meanLogFC)), na.rm=T)
+    warning("obs log FC == 0, will take the mean \n")
+  }
+  # emp_pval/length(shuff_logFC_TAD_region)
+  ### ADD THE +1 => THIS IS FOR THE OBSERVED VALUE -> AVOID THE 0 VALUES !!!
+  (emp_pval+1)/(length(shuff_logFC_TAD_region)+1)
+}
+names(emp_pval_meanLogFC) <- intersectRegions
+
+stopifnot(all(emp_pval_meanLogFC > 0 & emp_pval_meanLogFC <= 1 ))
+
+################################****************************************************************************************
+####################################################### WRITE OUTPUT
+################################****************************************************************************************
+
+save(emp_pval_meanLogFC, file= paste0(curr_outFold, "/emp_pval_meanLogFC.Rdata"))
+cat(paste0("... written: ", curr_outFold, "/emp_pval_meanLogFC.Rdata", "\n"))
+
+
+outFile <- paste0(curr_outFold, "/", "volcano_plot_empPval_meanLogFC.", plotType)
+do.call(plotType, list(outFile, height=ifelse(plotType=="png", 480, 7), width=ifelse(plotType=="png", 600, 10)))
+
+plot( x = obs_TADLogFC[intersectRegions], y = log10(emp_pval_meanLogFC[intersectRegions]),
+      pch=16, cex  = 0.7,
+      main = paste0("volcano plot - emp. p-val mean logFC"),
+      xlab="mean observed logFC",
+      ylab = "log10 (# permuted mean logFC <> observed mean log FC)")
+
+emp_pval_meanLogFC_rank <- rank(emp_pval_meanLogFC, ties="min")
+emp_pval_meanLogFC_rank <- emp_pval_meanLogFC_rank[order(emp_pval_meanLogFC_rank)]
+top10_x <- names(sort(obs_TADLogFC[intersectRegions], decreasing = T))[1:nTop]
+top10_y <- names(sort(log10(emp_pval_meanLogFC[intersectRegions])))[1:nTop]
+tads_to_plot <- union(top10_x, top10_y)
+text(x = obs_TADLogFC[tads_to_plot],
+     y = log10(emp_pval_meanLogFC[tads_to_plot]),
+     col ="red",
+     pos=4,
+     srt = 45,
+     labels = tads_to_plot)
+legend("bottomright", bty="n", legend=c(paste0("(Top", nTop, " - union x and y, unsorted)"), tads_to_plot), cex=0.8)  
+foo <- dev.off()
+cat(paste0("... written: ", outFile, "\n"))
+
+
+txt <- paste0(startTime, "\n", Sys.time(), "\n")
+printAndLog(txt, pipLogFile)
+cat(paste0("*** DONE: ", script_name, "\n"))
+
